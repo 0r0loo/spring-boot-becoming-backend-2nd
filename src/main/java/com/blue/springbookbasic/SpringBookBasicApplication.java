c9469f1d40d58f0a29428071577f885c9661ffdd
chotaegyu <chotg5592@gmail.com>
2025-01-22 23:41:37 +0900

    Initˆ

---
+++ src/main/java/com/blue/springbookbasic/SpringBookBasicApplication.java
@@ -0,0 +1,13 @@
+package com.blue.springbookbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBookBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBookBasicApplication.class, args);
+    }
+
+}
