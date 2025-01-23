e8250891c04b107491b1009bf7e2ec52bcb7d222
chotaegyu <chotg5592@gmail.com>
2025-01-24 01:17:30 +0900

    블로그 글 전체 조회 api

--- src/main/java/com/blue/springbookbasic/service/BlogService.java
+++ src/main/java/com/blue/springbookbasic/service/BlogService.java
@@ -6,6 +6,8 @@ import com.blue.springbookbasic.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor // final 이 붙거나 @NonNull 인 필드만 파라미터로 받는 생성자를 만들어줌
 @Service // 서비스 빈으로 등록
 public class BlogService {
@@ -15,5 +17,9 @@ public class BlogService {
     // 블로그 글 추가 메서드
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
